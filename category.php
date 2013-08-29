15d7670d523e7a5cf46a3479963538ade12c229d
Erdei Edit <info@editerdei.com>
2013-08-29 09:18:00 +0100

    The template for displaying Category pages

---
+++ category.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying Category pages.
+ *
+ * Used to display archive-type pages for posts in a category.
+ *
+ * @package Green Variation
+ * @since	Available since Release 1.0
+ */
+?>
+
+	<?php get_header(); ?>
+
+	<div id="content" class="content-index">
+
+		<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', 'excerpt' ); ?>
+		<?php endwhile; ?>
+			<?php get_template_part( 'content', 'navigation' ); ?>
+		<?php else : ?>
+			<article>
+				<h1 class=""><?php _e( 'Oops, Post Not Found!', THEME_NAME ); ?></h1>
+			</article>
+		<?php endif; ?>
+
+	</div>	<!-- end content -->
+
+	<?php get_sidebar() ?>
+
+	<?php get_footer(); ?>
